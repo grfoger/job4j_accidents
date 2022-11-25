0011bca197b6d1ad6dfb07bca632cd96a0820d48
Arhip <grfoger@gmail.com>
2022-11-25 10:13:22 +0300

    auto load test properties for check

--- src/test/java/ru/job4j/accidents/repositoryjdbc/JdbcTest.java
+++ src/test/java/ru/job4j/accidents/repositoryjdbc/JdbcTest.java
@@ -1,46 +1,21 @@
 package ru.job4j.accidents.repositoryjdbc;
 
-import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.Assert;
 import ru.job4j.accidents.model.Accident;
-import ru.job4j.accidents.repository.AccidentTypeMem;
-import ru.job4j.accidents.repository.RuleMem;
 
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
-
 
 public class JdbcTest {
 
     public final JdbcTemplate jdbc;
 
-    public JdbcTest() {
-        this.jdbc = new JdbcTemplate(ds(
-                "org.h2.Driver",
-                "jdbc:h2:./testdb;MODE=PostgreSQL;CASE_INSENSITIVE_IDENTIFIERS=TRUE;",
-                "",
-                ""
-        ));
+    public JdbcTest(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
     }
 
-
-    public DataSource ds(String driver, String url, String username, String password) {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(driver);
-        ds.setUrl(url);
-        ds.setUsername(username);
-        ds.setPassword(password);
-        return ds;
-    }
 
     @Test
     public void createAndGetTest() {
