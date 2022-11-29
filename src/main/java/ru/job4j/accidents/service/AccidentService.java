10ec76deec2c2140a50966a9ee2d2ba9de103828
grfoger <grfoger@gmail.com>
2022-11-29 23:40:38 +0300

    corrected service without tests

--- src/main/java/ru/job4j/accidents/service/AccidentService.java
+++ src/main/java/ru/job4j/accidents/service/AccidentService.java
@@ -2,12 +2,16 @@ package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.repository.AccidentMem;
 import ru.job4j.accidents.repository.AccidentTypeMem;
 import ru.job4j.accidents.repository.RuleMem;
+import ru.job4j.accidents.repositorydata.AccidentRepository;
+import ru.job4j.accidents.repositorydata.AccidentTypeRepository;
+import ru.job4j.accidents.repositorydata.RuleRepository;
 import ru.job4j.accidents.repositoryhbm.AccidentHibernate;
 import ru.job4j.accidents.repositoryhbm.AccidentTypeHibernate;
 import ru.job4j.accidents.repositoryhbm.RuleHibernate;
@@ -15,46 +19,54 @@ import ru.job4j.accidents.repositoryjdbc.AccidentJdbc;
 import ru.job4j.accidents.repositoryjdbc.AccidentTypeJdbc;
 import ru.job4j.accidents.repositoryjdbc.RuleJdbc;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AccidentService {
 
-    private final AccidentHibernate store;
-    private final AccidentTypeHibernate typeStore;
-    private final RuleHibernate ruleStore;
+    private final AccidentRepository store;
+    private final AccidentTypeRepository typeStore;
+    private final RuleRepository ruleStore;
 
 
     public Collection<Accident> getAll() {
-        return store.getAll();
+        List<Accident> result = new ArrayList<>();
+        store.findAll().forEach(result::add);
+        return result;
     }
 
     public void create(Accident accident) {
-        store.create(accident);
+        store.save(accident);
     }
 
     public void update(Accident accident) {
-        store.update(accident);
+        store.save(accident);
     }
 
     public Accident getById(int id) {
-        return store.getById(id);
+        return store.findById(id).get();
     }
 
     public Collection<AccidentType> getTypes() {
-        return typeStore.getTypes();
+        List<AccidentType> result = new ArrayList<>();
+        typeStore.findAll().forEach(result::add);
+        return result;
     }
 
     public AccidentType getTypeById(int id) {
-        return typeStore.getTypeById(id);
+        return typeStore.findById(id).get();
     }
 
     public Collection<Rule> getRules() {
-        return ruleStore.getRules();
+        List<Rule> result = new ArrayList<>();
+        ruleStore.findAll().forEach(result::add);
+        return result;
     }
 
     public Rule getRuleById(int id) {
-        return ruleStore.getRuleById(id);
+        return ruleStore.findById(id).get();
     }
 }
