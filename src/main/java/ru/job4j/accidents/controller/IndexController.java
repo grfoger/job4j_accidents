10a1a1c341a4653643cb52d562e2b957475fca89
Arhip <grfoger@gmail.com>
2022-12-14 17:05:36 +0300

    need correct db error

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,5 +1,6 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,26 +20,10 @@ public class IndexController {
     }
 
 
-    @GetMapping("/index")
+    @GetMapping(value = {"/index", "/", "/accidents"})
     public String index(Model model) {
-        model.addAttribute("user", "Arhip Myravsky");
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", service.getAll());
-        return "index";
-    }
-
-    @GetMapping("/")
-    public String indexBlank(Model model) {
-        model.addAttribute("user", "Arhip Myravsky");
-        model.addAttribute("accidents", service.getAll());
-        System.out.println(service.getAll());
-        return "index";
-    }
-
-    @GetMapping("/accidents")
-    public String accidents(Model model) {
-        model.addAttribute("user", "Arhip Myravsky");
-        model.addAttribute("accidents", service.getAll());
-        System.out.println(service.getAll());
         return "index";
     }
 }
