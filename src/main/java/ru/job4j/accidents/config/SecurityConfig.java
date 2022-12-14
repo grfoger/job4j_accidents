b32dafb33782d3de6456e78290028629e6a4ab2e
grfoger <grfoger@gmail.com>
2022-12-14 23:32:08 +0300

    1. Авторизация через JDBC [#2094 #371188] some new commit

--- src/main/java/ru/job4j/accidents/config/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.jdbcAuthentication()
                 .dataSource(ds);
     }
