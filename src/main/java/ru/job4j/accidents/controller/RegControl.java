a6252d892548b2fb43e5467cfbad1672390ad43e
grfoger <grfoger@gmail.com>
2022-12-30 07:51:08 +0300

    add registration exception handler

--- src/main/java/ru/job4j/accidents/controller/RegControl.java
+++ src/main/java/ru/job4j/accidents/controller/RegControl.java
@@ -3,9 +3,11 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repositorydata.AuthorityRepository;
 import ru.job4j.accidents.repositorydata.UserRepository;
@@ -22,12 +24,21 @@ public class RegControl {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
+        try {
+            users.save(user);
+        } catch (Exception e) {
+            return "redirect:/reg?error=true";
+        }
         return "redirect:/login";
     }
 
     @GetMapping("/reg")
-    public String regPage() {
+    public String regPage(@RequestParam(value = "error", required = false) String error,
+                          Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage",
+                    "Ошибка регистрации. Возможно такой пользователь уже существует.");
+        }
         return "reg";
     }
 }
