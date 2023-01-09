aee0f99d3c85d5b1f29cddc454a8f836c22b0f31
grfoger <grfoger@gmail.com>
2023-01-09 22:23:45 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/accidents/control/AccidentControlTest.java
+++ src/test/java/ru/job4j/accidents/control/AccidentControlTest.java
@@ -2,15 +2,23 @@ package ru.job4j.accidents.control;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -19,6 +27,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class AccidentControlTest {
+
+    @MockBean
+    private AccidentService service;
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,4 +42,20 @@ public class AccidentControlTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("createAccident"));
     }
+
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultPostMessage() throws Exception {
+        this.mockMvc.perform(post("/saveAccident")
+                        .param("name", "Происшествие")
+                        .param("type", "1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/index"));
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        verify(service).create(argument.capture());
+        assertThat(argument.getValue().getName(), is("Происшествие"));
+    }
+
 }
